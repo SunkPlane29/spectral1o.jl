a87ca8570fb428f493efee8eac80dcf1dce650e4
André <andrezito300@gmail.com>
2025-11-13 13:03:22 -0300

    pa is somewhat working for reasonable mr diagrams, but chebyshev is not...

--- scripts.jl
+++ scripts.jl
@@ -1,9 +1,11 @@
 function test_get_eos()
-    nparam = 5
-    a = 2.0
+    nparam = 11
+    a = 5.0
+    # a = 0.5
     param = -a .+ 2a .* rand(nparam)
     # param = -a .* rand(nparam)
-    p, e, cs2 = get_eos(param, "in/ska.table"; pmax=1e3, ncrust=200, logspacing=true)
+    pmax = 700.0
+    p, e, cs2 = get_eos(param, "in/ska.table"; pmax=pmax, ncrust=200, logspacing=true, pa_eos=true)
 
     fig = Figure()
 
@@ -24,4 +26,31 @@ function test_get_eos()
     lines!(ax2, e, cs2; color=:green, label="cs2 from Gamma")
 
     save("out/eos_test.png", fig)
+
+    pc_start = 0.1
+    pc_end = pmax
+    nstars = 100
+
+    pc, M, R, Lambda = getMRLambdadiagram(p, e, pc_start, pc_end, nstars)
+
+    fig2 = Figure()
+    ax3 = Axis(fig2[1, 1]; 
+        xlabel="R (km)", ylabel="M (M☉)",
+        xscale=identity, yscale=identity,
+    )
+
+    lines!(ax3, R, M; color=:red, label="M-R")
+
+    save("out/mr_test.png", fig2)
+
+    fig3 = Figure()
+    ax4 = Axis(fig3[1, 1];
+        xlabel="M (M☉)", ylabel="Λ",
+        xscale=identity, yscale=log10,
+        limits=(0, maximum(M), 1e1, 1e5),
+    )
+
+    lines!(ax4, M, abs.(Lambda); color=:purple, label="M-Λ")
+
+    save("out/mlambda_test.png", fig3)
 end
